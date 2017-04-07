1c08534b5bc9a6a4f01ff938ce06ad12cbb6631b
CirJohne <john.cirverius@agical.se>
2017-04-07 10:48:54 +0200

    styrt qwerty

--- keyboard/planck/keymaps/keymap_johneq.c
+++ keyboard/planck/keymaps/keymap_johneq.c
@@ -18,7 +18,7 @@
 
 #define THUMB_ROW {M(0), KC_RGUI, KC_LGUI, KC_LCTL, KC_LSFT, LT(_LW, KC_BSPC), LT(_RS, KC_SPC), LT(_I3, KC_ESC), KC_LALT, KC_DEL, KC_VOLD, KC_VOLU}
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_CM] = { /* Qwerty*/
+[_CM] = { /* Qwerty */
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   SV_AA  },
   {KC_APP,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SV_OE,    SV_AE  },
   {KC_BSPC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
